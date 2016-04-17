d0db32f69bf746c31732d47235a3d53a58495281
Jesse Atkinson <jesse.atkinson@me.com>
2016-04-17 11:10:40 -0700

    Add quick and dirty scroll-to functionality

--- js/main.js
+++ js/main.js
@@ -4,12 +4,29 @@
   var showHideNav = function () {
     $('.hamburger-button').on('click', function (event) {
       event.preventDefault();
+      // TODO: Merge this into one command
       $(this).toggleClass('active');
-      $('nav').toggle();
+      $('.wrapper.nav nav').toggleClass('active');
+    });
+  };
+
+  var scrollToSections = function () {
+    $('.wrapper.nav nav a[href^="#"]').on('click', function (event) {
+      event.preventDefault();
+      var $target = $($(this).attr('href'));
+
+      if ($target.length) {
+        $('html, body').animate({
+          scrollTop: $target.offset().top
+        }, 300);
+
+        $('.hamburger-button, .wrapper.nav nav').toggleClass('active');
+      }
     });
   };
 
   $(document).on('ready', function () {
     showHideNav();
+    scrollToSections();
   });
 })(jQuery, this);
