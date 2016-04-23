b0ed30c1f49f50ba1aa3a25c64459c67ae14349a
Jesse Atkinson <jesse.atkinson@me.com>
2016-04-23 14:01:07 -0700

    * Add Google Map * Clean up main.js and make it not look like a shit show

--- js/main.js
+++ js/main.js
@@ -1,79 +1,110 @@
 (function ($, window, undefined) {
   'use strict';
 
-  var breakpoint = $('.nav.desktop-only').offset().top;
-  var paddingTop = $('.nav.desktop-only').outerHeight(true);
+  // Global vars in closure
+  var desktopBreakpoint = 0;
+  var mobileBreakpoint  = 0;
+  var paddingTop        = 0;
+  var elms              = {}; // Object that holds all of our jQuery elements.
+
+  var cacheElms = function () {
+    elms.$window            = $(window);
+    elms.$body              = $('html, body');
+    elms.$hamburgerButton   = $('.hamburger-button');
+    elms.$mobileNav         = $('.wrapper.nav.mobile-only nav');
+    elms.$mobileNavWrapper  = $('.nav.mobile-only');
+    elms.$desktopNavWrapper = $('.nav.desktop-only');
+    elms.$scrollToTop       = $('.scroll-to-top');
+    elms.$scrollToSection   = $('.nav nav a[href^="#"]');
+    elms.$what              = $('.what');
+  };
+
+  var setGlobalVars = function () {
+    mobileBreakpoint  = elms.$mobileNavWrapper.offset().top;
+    desktopBreakpoint = elms.$desktopNavWrapper.offset().top;
+    paddingTop        = elms.$desktopNavWrapper.outerHeight(true);
+  };
+
+  var toggleActiveMobileNav = function () {
+    elms.$hamburgerButton.toggleClass('active');
+    elms.$mobileNav.toggleClass('active');
+  };
 
   var showHideNav = function () {
-    $('.hamburger-button').on('click', function (event) {
+    elms.$hamburgerButton.on('click', function (event) {
       event.preventDefault();
-      // TODO: Merge this into one command
-      $(this).toggleClass('active');
-      $('.wrapper.nav nav').toggleClass('active');
+      toggleActiveMobileNav();
     });
   };
 
   var scrollToSections = function () {
-    $('.scroll-to-top').on('click', function (event) {
+    elms.$scrollToTop.on('click', function (event) {
       event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
+      elms.$body.animate({scrollTop: 0}, 300);
     });
 
-    $('.nav nav a[href^="#"]').on('click', function (event) {
+    elms.$scrollToSection.on('click', function (event) {
       event.preventDefault();
+
+      // Get section to scroll to
       var $target = $($(this).attr('href'));
 
+      // If our target exists... Just a safety check.
       if ($target.length) {
-        $('html, body').animate({
+        elms.$body.animate({
           scrollTop: $target.offset().top - 50
         }, 300);
 
-        if ($('.nav.mobile-only').is(':visible')) {
-          $('.hamburger-button, .wrapper.nav nav').toggleClass('active');
+        if (elms.$mobileNavWrapper.is(':visible')) {
+          toggleActiveMobileNav();
         }
       }
     });
   };
 
   var transitionNavBarBackgroundOnMobile = function () {
-    if ($(window).scrollTop() > breakpoint) {
-      $('.mobile-only.nav').addClass('get-background-color');
+    if (elms.$window.scrollTop() > mobileBreakpoint) {
+      elms.$mobileNavWrapper.addClass('get-background-color');
     } else {
-      $('.mobile-only.nav').removeClass('get-background-color');
+      elms.$mobileNavWrapper.removeClass('get-background-color');
     }
   };
 
-  var nameMeDamnIt = function () {
-    if ($('.nav.desktop-only').is(':visible') &&  ($(window).scrollTop() > breakpoint)) {
-      $('.what').css({
+  var desktopStickNav = function () {
+    if (elms.$desktopNavWrapper.is(':visible') &&  (elms.$window.scrollTop() > desktopBreakpoint)) {
+      elms.$what.css({
         paddingTop: paddingTop + 'px'
       });
 
-      $('.nav.desktop-only').css({
+      elms.$desktopNavWrapper.css({
         position: 'fixed',
         width: '100%'
       });
     } else {
-      $('.what').css({
+      elms.$what.css({
         paddingTop: 0
       });
 
-      $('.nav.desktop-only').css({
+      elms.$desktopNavWrapper.css({
         position: 'relative',
         width: 'auto'
       });
     }
   };
 
+  var callOnWindowScroll = function () {
+    transitionNavBarBackgroundOnMobile();
+    desktopStickNav();
+  };
+
   $(document).on('ready', function () {
+    cacheElms();
+    setGlobalVars();
     showHideNav();
     scrollToSections();
     transitionNavBarBackgroundOnMobile();
-    nameMeDamnIt();
+    desktopStickNav();
 
-    $(window).on('scroll', function () {
-      transitionNavBarBackgroundOnMobile();
-      nameMeDamnIt();
-    });
+    elms.$window.on('scroll', callOnWindowScroll);
   });
 })(jQuery, this);
